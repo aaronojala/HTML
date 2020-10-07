930be6b53245f100b8ed0c03984d4fa52c771cf7
Aaron Ojala <aaronojala@yahoo.com>
2020-10-07 18:35:13 +0300

    back to top button

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,5 +1,25 @@
+const backToButton = document.getElementById('back-to-top');
+const width = window.matchMedia('(max-width: 576px)');
+
+// mobile menu
+
 const mobileMenu = () => {
-    const menu = document.querySelector('ul');
-  
-    menu.classList.toggle('mobilemenu');
-  };
+  const menu = document.querySelector('ul');
+
+  menu.classList.toggle('mobilemenu');
+};
+
+// back to top button
+
+window.onscroll = () => scrollCheck();
+
+const scrollCheck = () =>
+  document.body.scrollTop > 200 ||
+  (document.documentElement.scrollTop > 200 && !width.matches)
+    ? (backToButton.style.display = 'flex')
+    : (backToButton.style.display = 'none');
+
+const backToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
